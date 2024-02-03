230c6d68b0f8ac3ecc40592771a6679ae3f4bcf4
= <=>
2024-02-03 11:58:02 -0500

    Use add gin form tags to schema

--- internal/db/schema.go
+++ internal/db/schema.go
@@ -9,8 +9,8 @@ import (
 type Movie struct {
 	bun.BaseModel `bun:"table:movies,alias:m"`
 
-	ID          int64     `bun:",pk,autoincrement"`
-	Name        string    `bun:"name"`
+	ID          int64     `bun:",pk,autoincrement" form:"id"`
+	Name        string    `bun:"name" form:"name"`
 	AddedAt     time.Time `bun:"added_at"`
 	DigitizedAt time.Time `bun:"digitized_at"`
 }
