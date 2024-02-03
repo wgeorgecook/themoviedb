e2ed65dc38564b64186b737066afe05a779f1f8b
= <=>
2024-02-03 11:57:28 -0500

    Verbose query printing on debug mode

--- internal/db/client.go
+++ internal/db/client.go
@@ -7,12 +7,16 @@ import (
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
 )
 
 var client *bun.DB
 
-func Init(uri string) {
+func Init(uri string, debug bool) {
 	log.Printf("inititalizing DB: %s", uri)
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(uri)))
 	client = bun.NewDB(sqldb, pgdialect.New())
+	if debug {
+		client.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
+	}
 }
