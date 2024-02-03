2e2c5128f4aab641e155f18472073fd86e72a894
= <=>
2024-02-03 12:35:19 -0500

    Search the movie db if no matching movie found in db and insert all results

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"database/sql"
+	"log"
 	"moviedb/internal/db"
 	"net/http"
 
@@ -27,16 +28,33 @@ func getMovie(c *gin.Context) {
 		c.IndentedJSON(http.StatusBadRequest, httpError{Message: "request could not marshal"})
 		return
 	}
+
+	// query the db cache first
 	found, err := db.GetMovie(c.Request.Context(), movie)
-	if err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			c.IndentedJSON(http.StatusNotFound, httpError{Message: "movie not found"})
-			return
-		}
+	if err != nil && err != sql.ErrNoRows {
 		c.IndentedJSON(http.StatusInternalServerError, err)
 		return
 	}
-	c.IndentedJSON(http.StatusOK, found)
+	if found != nil {
+		c.IndentedJSON(http.StatusOK, found)
+		return
+	}
+
+	// search on themoviedb to get another movie
+	searchResults, err := movieDbClient.SearchMovies(c.Request.Context(), movie.Name)
+	if err != nil {
+		c.IndentedJSON(http.StatusFailedDependency, httpError{Message: err.Error()})
+		return
+	}
+	for _, result := range searchResults {
+		if err := db.UpsertMovie(c.Request.Context(), &db.Movie{
+			ID:   int64(result.ID),
+			Name: result.Title,
+		}); err != nil {
+			log.Printf("could not upsert %s: %s", result.Title, err.Error())
+		}
+	}
+
+	c.IndentedJSON(http.StatusOK, searchResults)
 
 }
